4ed1effaa18322563d185e21f2d5aa3b104c28b8
Eng. Moses Mangabo <mangabomoses@gmail.com>
2025-12-20 19:46:07 +0300

    java initializer files

---
+++ src/test/java/com/ujenzilink/ujenzilink_backend/UjenzilinkBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ujenzilink.ujenzilink_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UjenzilinkBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
